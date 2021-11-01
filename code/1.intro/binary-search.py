7f9cd24c8f6ce766de991075f6b7802e662f3945
Jonathas Rocha <62067474+jonathasrochadesouza@users.noreply.github.com>
2021-11-01 19:48:11 -0300

    📝Code 1: Introduction
    Binary search

---
+++ code/1.intro/binary-search.py
@@ -0,0 +1,19 @@
+def binary_search(lista, item):
+    baixo = 0
+    alto  = len(lista) - 1
+
+    while baixo <= alto:
+        meio = int((baixo + alto) / 2)
+        chute = lista[meio]
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+    return None
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 3))  # => 1
+print(binary_search(my_list, -1)) # => None
